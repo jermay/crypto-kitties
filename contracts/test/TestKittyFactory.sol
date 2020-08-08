e8ccb99a2d31a6a0258ac72bbb99f2d7ea22cf00
Jeremy May <may.jeremy.r@gmail.com>
2020-08-07 19:56:24 -0700

    contract kittiesOf() method

--- contracts/test/TestKittyFactory.sol
+++ contracts/test/TestKittyFactory.sol
@@ -9,4 +9,14 @@ contract TestKittyFactory is KittyFactory {
     function setOwnerKittyCount(address _address, uint256 _count) public {
         ownerKittyCount[_address] = _count;
     }
+
+    function createKitty(
+        uint256 _mumId,
+        uint256 _dadId,
+        uint256 _generation,
+        uint256 _genes,
+        address _owner
+    ) public returns (uint256) {
+        _createKitty(_mumId, _dadId, _generation, _genes, _owner);
+    }
 }
